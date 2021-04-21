571d572c0894401714ea263240a1de1dada01ddf
Inácio Nery <inacionery@gmail.com>
2021-04-21 20:41:28 -0300

    Add Spring Initializr

---
+++ src/main/java/com/inacionery/github/web/scraping/ScrapingApplication.java
@@ -0,0 +1,16 @@
+package com.inacionery.github.web.scraping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Inácio Nery
+ */
+@SpringBootApplication
+public class ScrapingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapingApplication.class, args);
+	}
+
+}
