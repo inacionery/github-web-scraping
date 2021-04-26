a0cd85d8d4d22153dbc1eba82f15403365980acb
Inácio Nery <inacionery@gmail.com>
2021-04-26 11:15:51 -0300

    Enable cache

--- src/main/java/com/inacionery/github/web/scraping/ScrapingApplication.java
+++ src/main/java/com/inacionery/github/web/scraping/ScrapingApplication.java
@@ -2,10 +2,12 @@ package com.inacionery.github.web.scraping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author Inácio Nery
  */
+@EnableCaching
 @SpringBootApplication
 public class ScrapingApplication {
 
